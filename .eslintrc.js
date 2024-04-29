52c24dfbeba1bf4d4b8871f124de20f8aa4ed561
taipeijapan <shohei.horike@gmail.com>
2024-04-29 12:15:50 +0900

    Add ESLint configuration and gitignore, new project setup may require linting rules and ignored files.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  extends: ["next/core-web-vitals", "prettier"],
+  plugins: [],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "{react,react-dom/**,react-router-dom}",
+            group: "builtin",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+  },
+};
